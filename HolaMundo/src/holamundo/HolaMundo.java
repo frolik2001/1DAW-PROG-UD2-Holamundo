31b1ebc538cb9eb53625cdbd85c6f4b1bf34d1cb
Eduardo A. Ponce <frolik2001@yahoo.es>
2017-01-17 20:04:25 +0100

    Primer commit

--- HolaMundo/src/holamundo/HolaMundo.java
+++ HolaMundo/src/holamundo/HolaMundo.java
@@ -2,7 +2,6 @@ package holamundo;
 
 public class HolaMundo {
 	public static void main (String[] args) {
-		System.out.println("Hola Mundo!!!");
-		
+		System.out.println("¡¡¡Hola Mundo!!!");
 	}
 }
